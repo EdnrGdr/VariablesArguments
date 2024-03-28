8e9e118b2d5932ee94f6c2946f87f9b2555e3148
edanu <edanurgider94@hotmail.com>
2024-03-28 13:52:30 +0300

    28.03.2024 13:52

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        int a =4;
+        System.out.println(a);
+
+        int b;
+        b=12;
+        System.out.println(b);
+
+        int c;
+        c = 2+2;
+        System.out.println(c);
+
+        int toplam= a + b+ c;
+        System.out.println(toplam);
+
+        System.out.println("Toplam:" + toplam );
+    }
+}
